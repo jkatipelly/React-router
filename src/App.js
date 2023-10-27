1d560e29f29384104b5e356ebcd9cd37e12ebfb8
Jalander Reddy <jkatipelly@yahoo.com>
2023-10-27 19:35:14 +0100

    Navigate programatically using buttons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { About } from "./Components/About";
 import { Home } from "./Components/Home";
 import { Navbar } from "./Components/Navbar";
+import { OrderSummary } from "./Components/OrderSummary";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="about" element={<About />}></Route>
+        <Route path="order-summary" element={<OrderSummary />}></Route>
       </Routes>
     </>
   );
